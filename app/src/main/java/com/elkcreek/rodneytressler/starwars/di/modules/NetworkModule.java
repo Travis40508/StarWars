a06e312d7d8a0a393b6b1724b399a67cdb986d1f
Travis Tressler <r.travis.tressler@gmail.com>
2018-09-22 14:01:00 -0400

    Network Setup is complete.

--- app/src/main/java/com/elkcreek/rodneytressler/starwars/di/modules/NetworkModule.java
+++ app/src/main/java/com/elkcreek/rodneytressler/starwars/di/modules/NetworkModule.java
@@ -1,7 +1,56 @@
 package com.elkcreek.rodneytressler.starwars.di.modules;
 
+import com.elkcreek.rodneytressler.starwars.repo.network.StarWarsApi;
+import com.elkcreek.rodneytressler.starwars.repo.network.StarWarsService;
+import com.elkcreek.rodneytressler.starwars.repo.network.StarWarsServiceImpl;
+
 import dagger.Module;
+import dagger.Provides;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 @Module
 public class NetworkModule {
+    private String baseUrl;
+
+    public NetworkModule(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    @Provides
+    OkHttpClient providesOkHttpClient() {
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BASIC);
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build();
+        return client;
+    }
+
+    @Provides
+    Retrofit providesRetrofit(OkHttpClient okHttpClient) {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
+                .build();
+
+        return retrofit;
+    }
+
+    @Provides
+    StarWarsApi providesStarWarsApi(Retrofit retrofit) {
+        StarWarsApi starWarsApi = retrofit.create(StarWarsApi.class);
+
+        return starWarsApi;
+    }
+
+    @Provides
+    StarWarsService providesStarWarsService(StarWarsApi starWarsApi) {
+        return new StarWarsServiceImpl(starWarsApi);
+    }
 }
